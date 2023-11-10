aa5b345fa417fa38264109b408345d83ef3d5ad0
vsoch <vsoch@users.noreply.github.com>
2023-11-09 23:42:22 -0700

    formatting
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- parse-responses.py
+++ parse-responses.py
@@ -1,13 +1,15 @@
 import csv
 import os
+
 import yaml
+
 here = os.path.dirname(os.path.abspath(__file__))
 
 responses = os.path.join(here, "responses.csv")
 
 rows = []
-with open(responses, newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+with open(responses, newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",", quotechar='"')
     for row in reader:
         rows.append(row)
 
@@ -17,8 +19,8 @@ max_score = 17
 # Row[-1] is feedback
 counts = {}
 for row in rows:
-    votes = row[1].replace('\\,', '[comma]')
-    noodles = votes.split(',')
+    votes = row[1].replace("\\,", "[comma]")
+    noodles = votes.split(",")
 
     # Give one point for rank, lowest score wins
     for i, noodle in enumerate(noodles):
@@ -27,15 +29,15 @@ for row in rows:
             counts[noodle] = 0
         counts[noodle] += score
 
-counts = sorted(counts.items(), reverse=True, key=lambda x:x[1])
+counts = sorted(counts.items(), reverse=True, key=lambda x: x[1])
 
 # Replace commas back in
 finals = {"items": []}
 for item in counts:
     key, count = item
-    key = key.replace('[comma]', ',').strip()
+    key = key.replace("[comma]", ",").strip()
     finals["items"].append({"name": key, "score": count})
 
 data_file = os.path.join(here, "_data", "scores.yaml")
-with open(data_file, 'w') as fd:
+with open(data_file, "w") as fd:
     fd.write(yaml.dump(finals))
